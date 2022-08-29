1d9a616840f79cdd1647c3ff60086d2e9bf6faf7
root <yessica_Stern@hotmail.com>
2022-08-29 17:51:53 -0300

    finalizado

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ server.use(express.json());
 
 const user=[];
 const tweets=[];
+
+let pageSize=10;
+function paginate(page=1){
+    return tweets.slice(pageSize*(page-1),pageSize*page);
+}
+
 server.get("/sign-up",(req,res)=>{
     res.send(user);
 });
@@ -15,13 +21,13 @@ server.post("/sign-up",(req,res)=>{
     res.send("OK");
 });
 server.get("/tweets",(req,res)=>{
-    res.send(tweets);
+    res.send(paginate(1));
 });
 server.post("/tweets",(req,res)=>{
     const tweet=req.body;
     const avatar=tweet.avatar;
     
-    tweets.push({...tweet, avatar:user[0].avatar});
+    tweets.unshift({...tweet, avatar:user[0].avatar});
     res.send("OK");
 });
 
