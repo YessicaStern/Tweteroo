c42a3cc5a3702acc839c34a95100c7f76ffa690c
root <yessica_Stern@hotmail.com>
2022-08-29 14:13:52 -0300

    express, cors e rotas implementadas

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 import express from "express";
 import cors from "cors";
 
+const server=express();
+server.use(cors());
+server.use(express.json());
+
+const user=[];
+const tweets=[];
+
+server.get("/sign-up",(req,res)=>{
+    res.send(user[0].avatar);
+});
+server.post("/sign-up",(req,res)=>{
+    user.push(req.body);
+    res.send("OK");
+});
+server.get("/tweets",(req,res)=>{
+    res.send(tweets);
+});
+server.post("/tweets",(req,res)=>{
+    const tweet=req.body;
+    const tweetAvatar=tweet.avatar;
+    tweets.push({...tweet, tweetAvatar:user[0].avatar});
+    res.send("OK TT");
+});
+
+server.listen(5000,()=>{console.log("Deu bom")});
